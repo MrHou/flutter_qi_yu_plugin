21e76d4e1ec2afa3669231f89a9bc8989ea78af5
hubert <houguanyuan@gmail.com>
2021-03-12 15:17:50 +0800

    feat:调试完成

--- example/android/app/src/main/kotlin/com/flutter/qy/flutter_qy_plugin_example/MainApplication.kt
+++ example/android/app/src/main/kotlin/com/flutter/qy/flutter_qy_plugin_example/MainApplication.kt
@@ -1,7 +1,13 @@
 package com.flutter.qy.flutter_qy_plugin_example
 
+import com.flutter.qy.flutter_qy_plugin.FlutterQyPlugin
 import io.flutter.app.FlutterApplication
 
 class MainApplication : FlutterApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        FlutterQyPlugin.initSDK(this,"063da122b2c0a592a70556647540efd5")
+
+    }
 
 }
